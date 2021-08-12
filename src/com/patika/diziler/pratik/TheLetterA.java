0b65e444af0960ee6c3825eddc6b693229da4b43
yasinbyzli <yasinbeeyazli@gmail.com>
2021-08-12 10:14:46 +0300

    the letter b

---
+++ src/com/patika/diziler/pratik/TheLetterA.java
@@ -0,0 +1,28 @@
+package com.patika.diziler.pratik;
+
+public class TheLetterA {
+    public static void main(String[] args) {
+        String[][] letter = new String[6][4];
+
+        // i -> Dizinin satırları
+        for (int i = 0; i < letter.length; i++) {
+            // j -> Dizinin sütunları
+            for (int j = 0; j < letter[i].length; j++) {
+                if(i == 0 || i == 2){
+                    letter[i][j] = " * ";
+                } else if(j == 0 || j == 3){
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
